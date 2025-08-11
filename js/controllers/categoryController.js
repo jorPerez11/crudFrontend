86a7ccd456ab24f28429cc9578be1c339f7dd260
fvp1233 <fernadomiguelvelasquezperez@gmail.com>
2025-08-11 15:45:45 -0600

    Avance por terminar

---
+++ js/controllers/categoryController.js
@@ -0,0 +1,118 @@
+import {
+    getCategories,
+    createCategory,
+    updateCategory,
+    deleteCategory
+} from "../services/categoryService.js";
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const tableBody = document.querySelector("#categoriesTable"); //Tbody- cuerpo de la tabla
+    const form = document.getElementById("categoryForm");//Formulario dentro del modal
+    const modal = new bootstrap.Modal(document.getElementById("categoryModal")); //Modal
+    const lblModal = document.getElementById("categoryModalLabel");//Titulo del modal
+    const btnAdd = document.getElementById("btnAddCategory"); //Boton para abrir modal
+
+    init();
+    
+    //Accion cuando el boton agregar nueva categoria es presionado(abrir Modal)
+    btnAdd.addEventListener("click", ()=> {
+        form.reset();
+        form.categoryId.value = ""; //No se manda el ID, porque estamos agregando
+        lblModal.textContent = "Agregar categoria";
+        modal.show();
+    });
+
+    form.addEventListener("submit", async(e)=>{
+        e.preventDefault(); //Evitamos que el formulario se envie al hacer "submit"
+        const id = form.categoryId.value; //Obtenemos el ID del registro, si no fuera de bootsrtrap utilizmos docuement.getelemntebyid
+        const data = {
+            nombreCategoria: form.categoryName.value.trim(),
+            descripcion: form.categoryDescription.value.trim() 
+        };
+
+        try{
+            //si hay un id significa que estamos actualizando
+            if(id){
+                await updateCategory(id,data);
+            }
+            //si no hay un id significa que estamos agrengando
+            else{
+                await createCategory(data);
+            }
+            modal.hide();//Se oculta el formulario despues de agregar/actualizar
+            await loadCategories(); //Nos permite cargar las categorias
+        } catch(err){
+            console.error("Error: " , err)
+        }
+    
+    });
+
+    async function loadCategories() {
+        try{
+            const categories = await getCategories();
+            tableBody.innerHTML = ""; //Vaciamos la tabla
+            if(!categories || categories.length == 0){
+                tableBody.innerHTML = '<td colspan=5>Actualmente no hay registros</td>';
+                return; //Evitemos que se ejecute el resto del codigo
+            }
+            categories.forEach((cat) =>{
+                const tr = document.createElement("tr"); //Se crea el elemento con js
+                tr.innerHTML = ` 
+                    <td>${cat.idCategoria}</td>
+                     <td>${cat.nombreCategoria}</td>
+                     <td>${cat.descripcion || ""}</td>
+                     <td>${cat.fechaCreacion || ""}</td>
+                     <td>
+                        <button class="btn btn-sm btn-outline-secondary edit-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-square-pen">
+                        <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                        <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/>
+                        </svg>
+                    </button>
+ 
+                    <button class="btn btn-sm btn-outline-danger delete-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-trash">
+                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+                        <path d="M3 6h18"/>
+                        <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+                        </svg>
+                    </button>
+                     </td>
+                `;
+
+                //Funcionalidad para botones de editar 
+                tr.querySelector(".edit-btn").addEventListener("click", ()=>{
+                    form.categoryId.value = cat.idCategoria;
+                    form.categoryName.value = cat.nombreCategoria;
+                    form.categoryDescription.value = cat.categoryDescription;
+                    lblModal.textContent= "Actualizar categoria";
+                    //El modal se carga hasta que el formulario ya tenga los datos cargados
+                    modal.show();
+                });
+
+                //Funcionabilidad para botones eliminar 
+                tr.querySelector(".delelete-btn").addEventListener("click", ()=>{
+                    if(confirm("¿Desea eliminar esta categoria?")){
+                        deleteCategory(cat.idCategoria).then(loadCategories);
+                    }
+
+                });
+
+                tableBody.appendChild(tr); //Al tb se le concatena la nueva fila creada
+            });
+
+        } catch(err){
+            console.error("Error cargando categorias: ", err);
+        }
+    }
+
+    function init(){
+        loadCategories();
+    }
+});
